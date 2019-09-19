4ded78f19f3c1a29ff544e1f0bb73061a6c08bff
skazko <kreeon@ya.ru>
2019-09-19 18:42:35 +0300

    react start

---
+++ react/precache-manifest.cf38f9a672391a7132c9937681af85c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d544c162134b00e155d8675556e3fa",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "d5e9ac0b041c3774b220",
+    "url": "/react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "be5568a36dd30b27ea34",
+    "url": "/react/static/css/main.ef196fec.chunk.css"
+  },
+  {
+    "revision": "d5e9ac0b041c3774b220",
+    "url": "/react/static/js/2.e84847e7.chunk.js"
+  },
+  {
+    "revision": "be5568a36dd30b27ea34",
+    "url": "/react/static/js/main.71b0be31.chunk.js"
+  },
+  {
+    "revision": "7a7d64b4a5bc6d3752cd",
+    "url": "/react/static/js/runtime~main.6993a003.js"
+  }
+]);
