2726ad0c43b9d3b91d12905b2192dc9df9191842
Slava <kreeon@ya.ru>
2020-02-07 13:12:00 +0300

    translate

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Привет</h1>
+    <p>Скоро здесь будет сайт заряженный Gatsby</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">На вторую страницу</Link>
   </Layout>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,9 +7,9 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>Страница 2</h1>
+    <p>А на ней тоже пока что пусто</p>
+    <Link to="/">Назад на главную</Link>
   </Layout>
 )
 
