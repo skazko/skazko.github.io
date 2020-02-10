cfa84f6f1fcd4f077f71f9602a1ad24af2221efc
Slava <kreeon@ya.ru>
2020-02-10 23:24:34 +0300

    add media

---
+++ src/styles/media.js
@@ -0,0 +1,9 @@
+const size = {
+  medium: '950px',
+  small: '640px'
+};
+
+export const screen = {
+  medium: `screen and (max-width: ${size.medium})`,
+  small: `screen and (max-width: ${size.small})`,  
+};
