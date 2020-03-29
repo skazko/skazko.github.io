700d6621a2b545ba4936207e278a93740763e267
Slava <kreeon@ya.ru>
2020-03-29 23:19:56 +0300

    fix handling playing video + update type of video

--- src/components/ProjectLink/ProjectLink.js
+++ src/components/ProjectLink/ProjectLink.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import style from './ProjectLink.module.css';
 
-const ProjectLink = ({ post }) => (
-  <li className={style.ProjectLink}>
-    <Link to={post.frontmatter.path}>
-      <video
-        onMouseEnter={e => e.target.play()}
-        onMouseLeave={e => e.target.pause()}
-        className={style.video} 
-        loop
-        muted>
-        <source src={post.frontmatter.video.publicURL} type="video/webm"/>
-      </video>
-    </Link>
-    <Link to={post.frontmatter.path}>
-      <h3>
-        {post.frontmatter.title}
-      </h3>
-    </Link>
-  </li>
-)
+const ProjectLink = ({ post }) => {
+  const [play, setPlay] = useState(false);
+  return (
+    <li className={style.ProjectLink}>
+      <Link to={post.frontmatter.path}>
+        <video
+          onMouseEnter={e => {
+            if (!play) {
+              setPlay(e.target.play());
+            }
+          }}
+          onMouseLeave={e => {
+            const target = e.target;
+            if (play) {
+              play.then(_ => {
+                target.pause();
+                setPlay(false);
+              })
+            }
+          }}
+          className={style.video} 
+          loop
+          muted
+          >
+          <source src={post.frontmatter.video.publicURL} type="video/mp4"/>
+        </video>
+      </Link>
+      <Link to={post.frontmatter.path}>
+        <h3>
+          {post.frontmatter.title}
+        </h3>
+      </Link>
+    </li>
+  )
+}
 
 export default ProjectLink
