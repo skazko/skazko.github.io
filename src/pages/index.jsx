1938c3bc4d6c68c0dcc35f36376ebab267d76996
Slava <kreeon@ya.ru>
2020-03-29 19:01:46 +0300

    add project list to index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
+import ProjectLink from '../components/ProjectLink';
+import ProjectList from '../components/ProjectList';
 
 export default (props) => {
+  const {data: {allMarkdownRemark: {edges}}} = props;
+  const Posts = edges
+    .filter(edge => edge.node.frontmatter.type === 'project')
+    .map(edge => <ProjectLink key={edge.node.id} post={edge.node} />);
+
   return (
     <Layout path={props.path}>
-      <p>Главная</p>
+      <h2 style={{marginBottom: '1.75rem'}}>Мои последние работы</h2>
+      <ProjectList>{Posts}</ProjectList>
     </Layout>
   )
 };
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}) {
+      edges {
+        node {
+          id
+          frontmatter {
+            type
+            date
+            path
+            title
+            video {publicURL}
+          }
+        }
+      }
+    }
+  }
+`;
