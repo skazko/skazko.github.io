cddf0778887e7fef662f8f5606c759be39060786
Slava <kreeon@ya.ru>
2020-03-31 12:02:17 +0300

    add contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,75 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 
-export default () => (
-  <Layout>
-    <p>Контакты</p>
-  </Layout>
+const SocialLink = ({item, children}) => (
+  <a
+    style={{fontSize: '1.5rem', fontWeight: '500'}}
+    title={`${item.name} - ${item.value}`}
+    href={item.link} 
+    target="_blank" 
+    rel="noopener noreferrer">
+    {children}
+  </a>
+)
+
+const Mail = ({mail}) => (
+  <div>
+    <p>Напишите мне письмо:</p>
+    <SocialLink item={mail}>
+      {mail.value}
+    </SocialLink>
+  </div>
 );
+
+const Telegram = ({tg}) => (
+  <div>
+    <p>или сообщение в Telegram:</p>
+    <SocialLink item={tg}>
+      @{tg.value}
+    </SocialLink>
+  </div>
+);
+
+const Github = ({gh}) => (
+  <div>
+    <p>Посмотрите мои работы на Github:</p>
+    <SocialLink item={gh}>
+      {gh.link.replace('https://', '')}
+    </SocialLink>
+  </div>
+);
+
+export default (props) => {
+  const contacts = props.data.site.siteMetadata.social;
+  const mail = contacts.find(item => item.name === 'email');
+  const github = contacts.find(item => item.name === 'github');
+  const telegram = contacts.find(item => item.name === 'telegram');
+  
+  return (
+    <Layout >
+      <h1 style={{fontSize: '2.5rem', textAlign: 'center'}}>Буду рад вашему сообщению</h1>
+      <div style={{textAlign: 'center'}}>
+        { mail ? <Mail mail={mail} /> : null }
+        { telegram ? <Telegram tg={telegram} /> : null }
+        { github ? <Github gh={github} /> : null }
+      </div>
+      
+    </Layout>
+  )
+};
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        social {
+          link
+          name
+          value
+        }
+      }
+    }
+  }
+`;
