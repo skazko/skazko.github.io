6d56f191cfde6f328a910d7f725a996d6e490acf
Slava <kreeon@ya.ru>
2020-05-05 19:03:44 +0300

    change template

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,49 +1,45 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import React from "react"
+import { graphql } from "gatsby"
 
-import Layout from '../components/Layout';
-import style from './blogTemplate.module.css';
+import Layout from "../components/Layout"
+import style from "./blogTemplate.module.css"
 
 export default function Template({ data }) {
-  const { markdownRemark } = data;
-  const { frontmatter, html } = markdownRemark;
+  const { markdownRemark } = data
+  const { frontmatter, html } = markdownRemark
 
   return (
     <Layout>
       <article className={style.project}>
         <h1>{frontmatter.title}</h1>
         <div className={style.links}>
-          <a
-            href={frontmatter.repo}
-            target="_blank"
-            rel="noopener noreferrer"
-          >Репозиторий</a> 
-          <a
-            href={frontmatter.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >Деплой</a>
+          <a href={frontmatter.repo} target="_blank" rel="noopener noreferrer">
+            Репозиторий
+          </a>
+          {frontmatter.url ? (
+            <a href={frontmatter.url} target="_blank" rel="noopener noreferrer">
+              Деплой
+            </a>
+          ) : null}
         </div>
         <video
-          className={style.video} 
+          className={style.video}
           loop
           autoPlay
           muted
           playsInline
           poster={frontmatter.poster.publicURL}
-          src={frontmatter.video.publicURL} >
-        </video>
-        <div 
-          dangerouslySetInnerHTML={{__html: html}}>
-        </div>
+          src={frontmatter.video.publicURL}
+        ></video>
+        <div dangerouslySetInnerHTML={{ __html: html }}></div>
       </article>
     </Layout>
-  );
+  )
 }
 
 export const pageQuery = graphql`
   query($path: String!) {
-    markdownRemark(frontmatter: {path: {eq: $path}}) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
         url
@@ -59,4 +55,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
